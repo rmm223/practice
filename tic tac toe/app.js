527a6a4d4e8b29e28ed94f356acfa57d28c70b34
rmm223 <rmm223@lehigh.edu>
2023-02-17 18:19:34 -0500

    first

---
+++ tic tac toe/app.js
@@ -0,0 +1,11 @@
+//Robin Maillard <rmm223@lehigh.edu>
+const express = require("express");
+const path = require("path");
+
+
+let app = express();
+
+let publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log("Starting up ..."));
